dc5e3d609dc7cd07915e7c53c3c814baa45578a5
yashtest1 <108034079+yashtest1@users.noreply.github.com>
2022-06-27 13:10:40 +0200

    Update app.py

--- data/app.py
+++ data/app.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# import the connect library from psycopg2
 from psycopg2 import connect
 
 table_name = "installation"
-
-# declare connection instance
 conn = connect(
     dbname = "awesomeinc",
     user = "interviewee",
     host = "172.28.1.4",
     password = "wannasucceed"
 )
-
-# declare a cursor object from the connection
 cursor = conn.cursor()
-
-# execute an SQL statement using the psycopg2 cursor object
 cursor.execute(f"select * from {table_name};")
-
-# enumerate() over the PostgreSQL records
 for i, record in enumerate(cursor):
     print ("\n", type(record))
     print ( record )
-
-# close the cursor object to avoid memory leaks
 cursor.close()
-
-# close the connection as well
 conn.close()
